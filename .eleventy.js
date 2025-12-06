d9dceaf953b056866fc792ec5c509667f5e405df
Lee Perry <14543108+lee-perry@users.noreply.github.com>
2025-12-06 23:13:54 +0000

    feeds temporarily removed

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
 const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
+module.exports.config = {
+  templateFormats: ["html", "liquid", "njk"],
+};
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets')
   return {
@@ -8,53 +12,12 @@ module.exports = function(eleventyConfig) {
   };
 }
 
-module.exports.config = {
-  templateFormats: ["html", "liquid", "njk", "md"],
-};
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(feedPlugin, {
-    type: "atom", // or "rss", "json"
-    outputPath: "/atom/",
-    collection: {
-      name: "all", // iterate over `collections.posts`
-      limit: 50,     // 0 means no limit
-    },
-    metadata: {
-      language: "en",
-      title: "lprry",
-      subtitle: "Dispatches from Burnley - Talking about running, cycling, learning code, dogs, our allotment, and Burnley FC",
-      base: "https://l.prry.uk",
-      author: {
-        name: "Lee Perry",
-        email: "lee@prry.uk", // Optional
-      }
-    }
-  });
-};
-
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(feedPlugin, {
-    type: "rss", // or "rss", "json"
-    outputPath: "/feed/.xml",
-    collection: {
-      name: "all", // iterate over `collections.posts`
-      limit: 50,     // 0 means no limit
-    },
-    metadata: {
-      language: "en",
-      title: "lprry",
-      subtitle: "Dispatches from Burnley - Talking about running, cycling, learning code, dogs, our allotment, and Burnley FC",
-      base: "https://l.prry.uk",
-      author: {
-        name: "Lee Perry",
-        email: "lee@prry.uk", // Optional
-      }
-    }
-  });
-};
-
-module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('assets')
+  return {
+  passthroughFileCopy: true
+  };
+  
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     // output image formats
     formats: ["webp", "jpeg"],
@@ -73,4 +36,6 @@ module.exports = function (eleventyConfig) {
       pictureAttributes: {}
     },
   });
+  
 };
+
