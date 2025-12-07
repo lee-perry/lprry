aa8ecb3c9b236659f68bbfb1380c1583f96f5de6
Lee Perry <14543108+lee-perry@users.noreply.github.com>
2025-12-07 21:31:01 +0000

    Granular feeds corrected/added

--- .eleventy.js
+++ .eleventy.js
@@ -125,25 +125,6 @@ module.exports = function (eleventyConfig) {
   
   eleventyConfig.addPlugin(feedPlugin, {
     type: "atom", // or "rss", "json"
-    outputPath: "/atom/articles/",
-    collection: {
-      name: "articless", // iterate over `collections.posts`
-      limit: 10,     // 0 means no limit
-    },
-    metadata: {
-      language: "en",
-      title: "lprry indieWeb garden - articles",
-      subtitle: "Burnley based, I talk about learning code and the indieWeb, staying fit by running, cycling, swimming and bouldering, retrogaming, our allotment, and life with our dogs and chickens.",
-      base: "https://l.prry.uk/",
-      author: {
-        name: "Lee Perry",
-        email: "lee@prry.uk", // Optional
-      }
-    }
-  });
-  
-  eleventyConfig.addPlugin(feedPlugin, {
-    type: "atom", // or "rss", "json"
     outputPath: "/atom/activities/",
     collection: {
       name: "training-log", // iterate over `collections.posts`
