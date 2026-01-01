8a1f5d7603af6ac9ddcb0d3661e97f031b2bb36f
Lee Perry <leeperry@Lees-MacBook-Pro.local>
2026-01-01 11:05:03 +0000

    Reinstating image-transforms

--- .eleventy.js
+++ .eleventy.js
@@ -32,7 +32,7 @@ module.exports = function (eleventyConfig) {
     extensions: "html",
   
     // output image widths
-    widths: [295, 445, 895, 1395],
+    widths: [300, 450, 900],
     heights: "auto",
   
     // optional, attributes assigned on <img> nodes override these values
